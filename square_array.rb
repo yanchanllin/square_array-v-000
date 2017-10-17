316db62fcd39ade5db87fe95b2f64a10e229d28c
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 20:02:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,19 @@
 def square_array(array)
-  counter = 1
-array.each do |number|
-counter += 1
-puts array.Square
- end
+  #new array right here. Let's call it result
+  result =[]
+  array.each do |number|
+    result << number**2
+  end
+  #returning the result array
+  result
 end
+# http://speakingjs.com/es5/ch15.html#_terminology_parameter_versus_argument
+#
+# square_array([2,5,7])
+#
+# map/collect
+# create an empty array
+# iterates through the parameter
+# does whatever code you tell it to do in the block
+# then it adds that result to the array from line 12
+# when the iteration over, returns the array from line 12
